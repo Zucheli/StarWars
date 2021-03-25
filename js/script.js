22ff57e93afe4dad81aef57a0197a72641fa196e
Mateus Zucheli <mateuszucheli@hotmail.com>
2021-03-25 17:15:42 -0300

    Dashboard

--- js/script.js
+++ js/script.js
@@ -6,6 +6,36 @@ const navesContador = document.getElementById("naves");
 preencherContadores();
 preencherTabela();
 
+google.charts.load("current", { packages: ["corechart"] });
+google.charts.setOnLoadCallback(desenharGrafico);
+
+async function desenharGrafico() {
+  const response = await swapiGet("films/");
+  const filmsArray = response.data.results;
+  console.log(filmsArray);
+
+  const dataArray = [];
+  dataArray.push(["Films", "Species"]);
+  filmsArray.forEach(film => {
+    dataArray.push([film.title, film.species.length]);
+  });
+
+  console.log(dataArray)
+
+  var data = google.visualization.arrayToDataTable(dataArray);
+
+  var options = {
+    title: "Species by Film",
+    legend: "none"
+  };
+
+  var chart = new google.visualization.PieChart(
+    document.getElementById("piechart")
+  );
+
+  chart.draw(data, options);
+}
+
 function preencherContadores() {
   Promise.all([
     swapiGet("people/"),
@@ -25,9 +55,9 @@ async function preencherTabela() {
   const tableData = response.data.results;
   tableData.forEach((film) => {
     $("#films-table").append(
-        `<tr>
+      `<tr>
             <td>${film.title}</td>
-            <td>${moment(film.release_date).format('DD/MM/YYYY')}</td>
+            <td>${moment(film.release_date).format("DD/MM/YYYY")}</td>
             <td>${film.director}</td>
             <td>${film.episode_id}</td>
         <tr>`
